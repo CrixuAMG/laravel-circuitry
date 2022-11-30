e1e473f936d011fd601c214187b64b7e8638d509
Christian Kaal <christiankaal@gmail.com>
2022-11-30 12:58:58 +0100

    Updated deps

---
+++ src/CircuitryServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CrixuAMG\Circuitry;
+
+use Illuminate\Support\ServiceProvider;
+
+class CircuitryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+    }
+}
